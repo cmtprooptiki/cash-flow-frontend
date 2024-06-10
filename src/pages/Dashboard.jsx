031deab0feb8ca12048ca75e564ab9d554a3c86d
Ippokratis Geranakis <igeran@cmt.local>
2024-06-10 13:16:18 +0300

    message

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,8 @@ import Welcome from '../components/Welcome';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getMe } from '../features/authSlice';
-
+import Esoda from './Esoda';
+import Eksoda from './Eksoda';
 import axios from 'axios';
 import ApexCharts from 'react-apexcharts';
 import Select from 'react-select';
@@ -32,8 +33,6 @@ import WeeksTable from '../components/WeeksTable'; // Import the WeeksTable comp
 
 const localizer = momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(Calendar);
-
-
 
 const Dashboard = () => {
   //Kodikas ippo
@@ -168,260 +167,262 @@ const Dashboard = () => {
     
   // );
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { isError } = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const { isError } = useSelector((state) => state.auth);
 
-  const [calendarDate, setCalendarDate] = useState(new Date());
-  const [selectedDateType, setSelectedDateType] = useState('estimate_payment_date');
-  const [eventClickedFirst, setEventClickedFirst] = useState(false);
-  const [boxData, setBoxData] = useState([]);
-  const [paradotea, setParadotea] = useState([]);
-  const [erganames, setErgaListNames] = useState([]);
-  const [selectedButton, setSelectedButton] = useState(null);
-  const [prevSelectedButton, setPrevSelectedButton] = useState(null);
-  const [income_paradotea,setIncome_paradotea]=useState([])
-  const [income_ekx,setIncome_ekx]=useState([])
-  const [income_ekx_cust, setIncome_Ekx_Cust] = useState([])
+  // const [calendarDate, setCalendarDate] = useState(new Date());
+  // const [selectedDateType, setSelectedDateType] = useState('estimate_payment_date');
+  // const [eventClickedFirst, setEventClickedFirst] = useState(false);
+  // const [boxData, setBoxData] = useState([]);
+  // const [paradotea, setParadotea] = useState([]);
+  // const [erganames, setErgaListNames] = useState([]);
+  // const [selectedButton, setSelectedButton] = useState(null);
+  // const [prevSelectedButton, setPrevSelectedButton] = useState(null);
+  // const [income_paradotea,setIncome_paradotea]=useState([])
+  // const [income_ekx,setIncome_ekx]=useState([])
+  // const [income_ekx_cust, setIncome_Ekx_Cust] = useState([])
   
   
 
-  useEffect(() => {
-    dispatch(getMe());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getMe());
+  // }, [dispatch]);
 
-  useEffect(() => {
-    if (isError) {
-      navigate('/');
-    }
-  }, [isError, navigate]);
+  // useEffect(() => {
+  //   if (isError) {
+  //     navigate('/');
+  //   }
+  // }, [isError, navigate]);
 
-  useEffect(() => {
-    getParadotea();
-    getErgaListNames();
-    getIncome_paradotea();
-    getIncome_ekx();
-    getIncome_ekx_cust_date();
-  }, []);
+  // useEffect(() => {
+  //   getParadotea();
+  //   getErgaListNames();
+  //   getIncome_paradotea();
+  //   getIncome_ekx();
+  //   getIncome_ekx_cust_date();
+  // }, []);
 
-  const getParadotea = async () => {
-    const response = await axios.get(`${apiBaseUrl}/getlistParErgColors`);
-    setParadotea(response.data);
-  };
+  // const getParadotea = async () => {
+  //   const response = await axios.get(`${apiBaseUrl}/getlistParErgColors`);
+  //   setParadotea(response.data);
+  // };
 
-  const getIncome_paradotea=async () =>{
-    const response = await axios.get(`${apiBaseUrl}/CheckParadotea`);
-    setIncome_paradotea(response.data);
-    //console.log(response.data)
-  }
-  const getIncome_ekx=async () =>{
-    const response = await axios.get(`${apiBaseUrl}/ParadoteaBank_Date`);
-    setIncome_ekx(response.data);
-    //console.log(response.data)
-  }
+  // const getIncome_paradotea=async () =>{
+  //   const response = await axios.get(`${apiBaseUrl}/CheckParadotea`);
+  //   setIncome_paradotea(response.data);
+  //   //console.log(response.data)
+  // }
+  // const getIncome_ekx=async () =>{
+  //   const response = await axios.get(`${apiBaseUrl}/ParadoteaBank_Date`);
+  //   setIncome_ekx(response.data);
+  //   //console.log(response.data)
+  // }
 
-  const getErgaListNames = async () => {
-    const response = await axios.get(`${apiBaseUrl}/getlistErgaNames`);
-    setErgaListNames(response.data);
-  };
+  // const getErgaListNames = async () => {
+  //   const response = await axios.get(`${apiBaseUrl}/getlistErgaNames`);
+  //   setErgaListNames(response.data);
+  // };
 
 
-  const getIncome_ekx_cust_date = async () =>
-    {
-      const response = await axios.get(`${apiBaseUrl}/ParadoteaCust_Date`);
-      setIncome_Ekx_Cust(response.data);
-    }
+  // const getIncome_ekx_cust_date = async () =>
+  //   {
+  //     const response = await axios.get(`${apiBaseUrl}/ParadoteaCust_Date`);
+  //     setIncome_Ekx_Cust(response.data);
+  //   }
 
-  const handleYearChange = (increment) => {
-    const newDate = new Date(calendarDate);
-    newDate.setFullYear(calendarDate.getFullYear() + increment);
-    setCalendarDate(newDate);
+  // const handleYearChange = (increment) => {
+  //   const newDate = new Date(calendarDate);
+  //   newDate.setFullYear(calendarDate.getFullYear() + increment);
+  //   setCalendarDate(newDate);
 
-  };
+  // };
 
-  const handleDateChange = (newDate) => {
-    setCalendarDate(newDate);
-  };
+  // const handleDateChange = (newDate) => {
+  //   setCalendarDate(newDate);
+  // };
 
   
 
-  const handleEventClick = (event, item) => {
-    setEventClickedFirst(true);
-    setBoxData(item);
-  };
+  // const handleEventClick = (event, item) => {
+  //   setEventClickedFirst(true);
+  //   setBoxData(item);
+  // };
 
-  const handleEventDrop = async ({ event, start, end }) => {
-    const updatedDate = moment.utc(start).startOf('day').toDate();
-    const dateTypeKey = selectedDateType;
+  // const handleEventDrop = async ({ event, start, end }) => {
+  //   const updatedDate = moment.utc(start).startOf('day').toDate();
+  //   const dateTypeKey = selectedDateType;
 
-    try {
-      console.log(event.item.id)
-      const response = await axios.patch(`${apiBaseUrl}/paradotea/${event.item.id}`, {
-        [dateTypeKey]: updatedDate
-      });
-      if (response.status === 200) {
-        setIncome_paradotea((prev) =>
-          prev.map((item) =>
-            item.paradotea.id === event.item.id ? { ...item, paradotea: { ...item.paradotea, [dateTypeKey]: updatedDate } } : item
-          )
-        );
-      }
-    } catch (error) {
-      console.error('Failed to update event', error);
-    }
-  };
+  //   try {
+  //     console.log(event.item.id)
+  //     const response = await axios.patch(`${apiBaseUrl}/paradotea/${event.item.id}`, {
+  //       [dateTypeKey]: updatedDate
+  //     });
+  //     if (response.status === 200) {
+  //       setIncome_paradotea((prev) =>
+  //         prev.map((item) =>
+  //           item.paradotea.id === event.item.id ? { ...item, paradotea: { ...item.paradotea, [dateTypeKey]: updatedDate } } : item
+  //         )
+  //       );
+  //     }
+  //   } catch (error) {
+  //     console.error('Failed to update event', error);
+  //   }
+  // };
 
-  const handleDateTypeChange = (dateType) => {
-    setSelectedDateType(dateType);
+  // const handleDateTypeChange = (dateType) => {
+  //   setSelectedDateType(dateType);
     
-    setSelectedButton(dateType);
-  };
+  //   setSelectedButton(dateType);
+  // };
 
-  const getDateBySelectedType = (item) => {
-    switch (selectedDateType) {
-      case 'estimate_payment_date_2':
-        return new Date(item.estimate_payment_date_2);
-      case 'estimate_payment_date_3':
-        return new Date(item.estimate_payment_date_3);
-      default:
-        return new Date(item.estimate_payment_date);
-    }
-  };
-  //console.log(income_paradotea)
-  // const MyEvents=paradotea.map((item) => ({
+  // const getDateBySelectedType = (item) => {
+  //   switch (selectedDateType) {
+  //     case 'estimate_payment_date_2':
+  //       return new Date(item.estimate_payment_date_2);
+  //     case 'estimate_payment_date_3':
+  //       return new Date(item.estimate_payment_date_3);
+  //     default:
+  //       return new Date(item.estimate_payment_date);
+  //   }
+  // };
+  // //console.log(income_paradotea)
+  // // const MyEvents=paradotea.map((item) => ({
+  // //   id: item.ekxorimena_timologia_id,
+  // //   title: (
+  // //     <div>
+  // //       <div
+  // //         className="circle"
+  // //         style={{
+  // //           backgroundColor: item.erga.color,
+  // //           boxShadow: '0px 0px 4px 2px ' + item.erga.color,
+  // //         }}
+  // //       ></div>
+  // //       {item.ammount_total} €
+  // //     </div>
+  // //   ),
+  // //   start: getDateBySelectedType(item),
+  // //   end: getDateBySelectedType(item),
+  // //   item: item,
+  // // }));
+  // //console.log(MyEvents)
+
+  // const MyEvents=income_paradotea.map((item) => ({
+  //     id: item.ekxorimena_timologia_id,
+  //     title: (
+  //       <div>
+  //         <div
+  //           className="circle"
+  //           style={{
+  //             backgroundColor: item.paradotea.erga.color,
+  //             boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
+  //           }}
+  //         ></div>
+  //         {item.paradotea.ammount_total} €
+  //       </div>
+  //     ),
+  //     start: getDateBySelectedType(item.paradotea),
+  //     end: getDateBySelectedType(item.paradotea),
+  //     item: item.paradotea,
+  //   }));
+  //   console.log("My Event : ",MyEvents)
+
+
+
+  //  const ekx=(income_ekx.map((item) => ({
   //   id: item.ekxorimena_timologia_id,
   //   title: (
-  //     <div>
+  //     <div style = {{backgroundColor: "green"}}>
   //       <div
   //         className="circle"
   //         style={{
-  //           backgroundColor: item.erga.color,
-  //           boxShadow: '0px 0px 4px 2px ' + item.erga.color,
+  //           backgroundColor: item.paradotea.erga.color,
+  //           boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
   //         }}
   //       ></div>
-  //       {item.ammount_total} €
+  //       {/* {console.log(item.Ekxorimena_Timologium.bank_ammount)} */}
+  //       {item.Ekxorimena_Timologium.bank_ammount} €
   //     </div>
   //   ),
-  //   start: getDateBySelectedType(item),
-  //   end: getDateBySelectedType(item),
+  //   start: new Date(item.Ekxorimena_Timologium.bank_date),
+  //   end: new Date(item.Ekxorimena_Timologium.bank_date),
   //   item: item,
-  // }));
-  //console.log(MyEvents)
+  // })));
 
-  const MyEvents=income_paradotea.map((item) => ({
-      id: item.ekxorimena_timologia_id,
-      title: (
-        <div>
-          <div
-            className="circle"
-            style={{
-              backgroundColor: item.paradotea.erga.color,
-              boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
-            }}
-          ></div>
-          {item.paradotea.ammount_total} €
-        </div>
-      ),
-      start: getDateBySelectedType(item.paradotea),
-      end: getDateBySelectedType(item.paradotea),
-      item: item.paradotea,
-    }));
-    console.log("My Event : ",MyEvents)
+  // const ekx_cust=(income_ekx_cust.map((item) => ({
+  //   id: item.ekxorimena_timologia_id,
+  //   title: (
+  //     <div style = {{backgroundColor: "green"}}>
+  //       <div
+  //         className="circle"
+  //         style={{
+  //           backgroundColor: item.paradotea.erga.color,
+  //           boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
+  //         }}
+  //       ></div>
+  //       {/* {console.log(item.Ekxorimena_Timologium.customer_ammount)} */}
+  //       {item.Ekxorimena_Timologium.customer_ammount} €
+  //     </div>
+  //   ),
+  //   start: new Date(item.Ekxorimena_Timologium.cust_date),
+  //   end: new Date(item.Ekxorimena_Timologium.cust_date),
+  //   item: item,
+  // })));
 
+  // function joinjson(items){
+  //   MyEvents.push(items)
+  // }
 
+  // const uniqueekx = [
+  //   ...ekx
+  // ].filter((event, index, self) =>
+  //   index === self.findIndex((e) => e.id === event.id)
+  // );
 
-   const ekx=(income_ekx.map((item) => ({
-    id: item.ekxorimena_timologia_id,
-    title: (
-      <div style = {{backgroundColor: "green"}}>
-        <div
-          className="circle"
-          style={{
-            backgroundColor: item.paradotea.erga.color,
-            boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
-          }}
-        ></div>
-        {/* {console.log(item.Ekxorimena_Timologium.bank_ammount)} */}
-        {item.Ekxorimena_Timologium.bank_ammount} €
-      </div>
-    ),
-    start: new Date(item.Ekxorimena_Timologium.bank_date),
-    end: new Date(item.Ekxorimena_Timologium.bank_date),
-    item: item,
-  })));
+  // const uniqueekx_cust = [
+  //   ...ekx_cust
+  // ].filter((event, index, self) =>
+  //   index === self.findIndex((e) => e.id === event.id)
+  // );
+  // uniqueekx.forEach(joinjson)
+  // uniqueekx_cust.forEach(joinjson)
 
-  const ekx_cust=(income_ekx_cust.map((item) => ({
-    id: item.ekxorimena_timologia_id,
-    title: (
-      <div style = {{backgroundColor: "green"}}>
-        <div
-          className="circle"
-          style={{
-            backgroundColor: item.paradotea.erga.color,
-            boxShadow: '0px 0px 4px 2px ' + item.paradotea.erga.color,
-          }}
-        ></div>
-        {/* {console.log(item.Ekxorimena_Timologium.customer_ammount)} */}
-        {item.Ekxorimena_Timologium.customer_ammount} €
-      </div>
-    ),
-    start: new Date(item.Ekxorimena_Timologium.cust_date),
-    end: new Date(item.Ekxorimena_Timologium.cust_date),
-    item: item,
-  })));
-
-  function joinjson(items){
-    MyEvents.push(items)
-  }
-
-  const uniqueekx = [
-    ...ekx
-  ].filter((event, index, self) =>
-    index === self.findIndex((e) => e.id === event.id)
-  );
-
-  const uniqueekx_cust = [
-    ...ekx_cust
-  ].filter((event, index, self) =>
-    index === self.findIndex((e) => e.id === event.id)
-  );
-  uniqueekx.forEach(joinjson)
-  uniqueekx_cust.forEach(joinjson)
-
-  // console.log("Ekxkxkxkx",ekx)
-  //console.log(MyEvents)
+  // // console.log("Ekxkxkxkx",ekx)
+  // //console.log(MyEvents)
 
   
 
-  const newparat = income_paradotea.map(item => ({
-    id: item.paradotea.id,
-    part_number: item.paradotea.part_number,
-    title: item.paradotea.title,
-    delivery_date: item.paradotea.delivery_date,
-    percentage: item.paradotea.percentage,
-    ammount: item.paradotea.ammount,
-    ammount_vat: item.paradotea.ammount_vat,
-    ammount_total: item.paradotea.ammount_total,
-    estimate_payment_date: item.paradotea.estimate_payment_date,
-    estimate_payment_date_2: item.paradotea.estimate_payment_date_2,
-    estimate_payment_date_3: item.paradotea.estimate_payment_date_3,
-    createdAt: item.paradotea.createdAt,
-    updatedAt: item.paradotea.updatedAt,
-    erga_id: item.paradotea.erga_id,
-    timologia_id: item.paradotea.timologia_id,
-    erga: {
-      name: item.paradotea.erga.name,
-      id: item.paradotea.erga.id,
-      color: item.paradotea.erga.color
-    }
-  }));
+  // const newparat = income_paradotea.map(item => ({
+  //   id: item.paradotea.id,
+  //   part_number: item.paradotea.part_number,
+  //   title: item.paradotea.title,
+  //   delivery_date: item.paradotea.delivery_date,
+  //   percentage: item.paradotea.percentage,
+  //   ammount: item.paradotea.ammount,
+  //   ammount_vat: item.paradotea.ammount_vat,
+  //   ammount_total: item.paradotea.ammount_total,
+  //   estimate_payment_date: item.paradotea.estimate_payment_date,
+  //   estimate_payment_date_2: item.paradotea.estimate_payment_date_2,
+  //   estimate_payment_date_3: item.paradotea.estimate_payment_date_3,
+  //   createdAt: item.paradotea.createdAt,
+  //   updatedAt: item.paradotea.updatedAt,
+  //   erga_id: item.paradotea.erga_id,
+  //   timologia_id: item.paradotea.timologia_id,
+  //   erga: {
+  //     name: item.paradotea.erga.name,
+  //     id: item.paradotea.erga.id,
+  //     color: item.paradotea.erga.color
+  //   }
+  // }));
   
  
 
   return (
     <Layout>
       <Welcome />
-      <h1 className = "Scenario" style = {{textAlign: "center", marginBottom: 20}}>{selectedButton === 'estimate_payment_date' ? 'Best-Case Scenario' :
+      <Esoda />
+      <Eksoda/>
+      {/* <h1 className = "Scenario" style = {{textAlign: "center", marginBottom: 20}}>{selectedButton === 'estimate_payment_date' ? 'Best-Case Scenario' :
          selectedButton === 'estimate_payment_date_2' ? 'Medium-Case Scenario' :
          selectedButton === 'estimate_payment_date_3' ? 'Worst-Case Scenario' : ''}</h1>
       <div className="container">
@@ -500,9 +501,9 @@ const Dashboard = () => {
   />
 )}
             {/* <WeeksTable income_paradotea={income_paradotea} selectedDateType={selectedDateType} calendarDate={calendarDate} onDateChange={handleDateChange}/> */}
-          </div>
+          {/* </div>
         </div>
-      </div>
+      </div>} */}
     </Layout>
   );
   
