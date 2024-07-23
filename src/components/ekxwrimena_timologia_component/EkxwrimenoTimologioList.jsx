1dd70df35ae4e2ae38bd9026b65deed5fad0d4ba
A-PALIOS <paliosadonis6@gmail.com>
2024-07-23 17:28:50 +0300

    message

--- src/components/ekxwrimena_timologia_component/EkxwrimenoTimologioList.jsx
+++ src/components/ekxwrimena_timologia_component/EkxwrimenoTimologioList.jsx
@@ -5,14 +5,81 @@ import { useSelector } from 'react-redux';
 import '../../buildinglist.css';
 import apiBaseUrl from '../../apiConfig';
 
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { InputText } from 'primereact/inputtext';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputNumber } from 'primereact/inputnumber';
+
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+import { Calendar } from 'primereact/calendar';
+import { Tag } from 'primereact/tag';
+
 const EkxwrimenoTimologioList = () => 
 {
     const [EkxwrimenoTimologio, setEkxorimena_Timologia] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [filters, setFilters] = useState(null);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+    
+
+
 
     const {user} = useSelector((state) => state.auth);
-    
+    const initFilters = () => {
+        setFilters({
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            id: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+            timologia_id: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+            bank_ammount: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+
+            bank_ammount: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            customer_ammount: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+
+            bank_estimated_date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+            bank_date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+            cust_estimated_date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+            cust_date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+
+        });
+        setGlobalFilterValue('');
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-between">
+                {/* <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined onClick={clearFilter} />
+                <IconField iconPosition="left">
+                    <InputIcon className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                </IconField> */}
+            </div>
+        );
+    };
+    const header = renderHeader();
+
+    const formatDate = (value) => {
+        let date = new Date(value);
+        if (!isNaN(date)) {
+            return date.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            });
+        } else {
+            return "Invalid date";
+        }
+    };
+
     useEffect(()=>{
         getEkxorimena_Timologia()
+        setLoading(false);
+        initFilters();
     }, []);
 
     const getEkxorimena_Timologia = async() =>{
@@ -26,65 +93,21 @@ const EkxwrimenoTimologioList = () =>
     };
 
     return(
-    <div>
-        <h1 className='title'>ΕΚΧΩΡΗΜΕΝΑ ΤΙΜΟΛΟΓΙΑ</h1>
+        <div className="card" >
+        <h1 className='title'>Εκχωρημένα τιμολόγια</h1>
         {user && user.role ==="admin" && (
-        <Link to={"/ek_tim/add"} className='button is-primary mb-2'>Προσθήκη Νέου Εκχωρημένου Τιμολογίου</Link>
+        <Link to={"/ek_tim/add"} className='button is-primary mb-2'><Button label="Προσθήκη Νέου Εκχωρημένου Τιμολογίου" icon="pi pi-plus-circle"/></Link>
         )}
-        <div className="table-responsive-md">
+        <DataTable value={EkxwrimenoTimologio} paginator showGridlines rows={10} scrollable scrollHeight="400px" loading={loading} dataKey="id" 
+                filters={filters} globalFilterFields={['id'
+                    ,'timologia_id','bank_ammount'
+                    ,'bank_estimated_date', 'bank_date', 'customer_ammount'
+                    ,'cust_estimated_date','cust_date']} header={header}
+                emptyMessage="No customers found.">
+                <Column field="id" header="id" filter filterPlaceholder="Search by name" style={{ minWidth: '5rem' }} />
+                <Column field="timologia_id" header="timologia_id" filter filterPlaceholder="Search by timologia_id" style={{ minWidth: '5rem' }} />
 
-            <table className='table is-stripped is-fullwidth'>
-                <thead>
-                    <tr>
-
-                        <th>#</th>
-
-                        <th>ΤΙΜΟΛΟΓΙΟ ID</th>
-                        <th>ΠΟΣΟ ΤΡΑΠΕΖΑΣ</th>
-                        <th>ΕΚΤΙΜΩΜΕΝΗ ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ ΤΡΑΠΕΖΑΣ</th>
-                        <th>ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ ΤΡΑΠΕΖΑΣ</th>
-                        
-
-                        <th>ΠΟΣΟ ΠΕΛΑΤΗ</th>
-                        <th>ΕΚΤΙΜΩΜΕΝΗ ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ ΠΕΛΑΤΗ</th>
-                        <th>ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ ΠΕΛΑΤΗ</th>
-                     
-
-                        <th>Ενέργειες</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {EkxwrimenoTimologio.map((EkxwrimenoTimologio,index)=>(
-                        <tr key={EkxwrimenoTimologio.id}>
-                            <td>{index+1}</td>
-                            <td>{EkxwrimenoTimologio.timologia_id}</td>
-                            <td>{EkxwrimenoTimologio.bank_ammount}</td>
-                            <td>{EkxwrimenoTimologio.bank_estimated_date }</td>
-                            <td>{EkxwrimenoTimologio.bank_date }</td>
-                           
-                            <td>{EkxwrimenoTimologio.customer_ammount }</td>
-                            <td>{EkxwrimenoTimologio.cust_estimated_date }</td>
-                            <td>{EkxwrimenoTimologio.cust_date }</td>
-                       
-
-
-                            <td>
-                                <Link to={`/ek_tim/profile/${EkxwrimenoTimologio.id}`} className='button is-small is-info'>Προφίλ</Link>
-                                {user && user.role ==="admin" && (
-                                <div>
-                                    <Link to={`/ek_tim/edit/${EkxwrimenoTimologio.id}`} className='button is-small is-info'>Επεξεργασία</Link>
-                                    <button onClick={()=>deleteEkxorimeno_Timologio(EkxwrimenoTimologio.id)} className='button is-small is-danger'>Διαγραφή</button>
-                                
-                                </div>
-                                )}
-                            
-                            </td>
-                    </tr>
-                ))}
-                
-                </tbody>
-            </table>
-        </div>
+           </DataTable>
     </div>
     );
 }
