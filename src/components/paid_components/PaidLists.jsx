8f6f08f8c730197b43e7ee782a78781da222900d
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-07-23 14:34:11 +0300

    message

--- src/components/paid_components/PaidLists.jsx
+++ src/components/paid_components/PaidLists.jsx
@@ -1,87 +1,218 @@
-import React,{useState,useEffect} from 'react'
-import {Link} from "react-router-dom"
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 import '../../buildinglist.css';
 import apiBaseUrl from '../../apiConfig';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { Calendar } from 'primereact/calendar';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dropdown } from 'primereact/dropdown';
+import { Tag } from 'primereact/tag';
 
 const PaidList = () => {
-    const [paradotea, setParadotea] = useState([]);
-    const {user} = useSelector((state)=>state.auth)
-    useEffect(()=>{
-        getParadotea()
-    },[]);
+    const [paradotea, setIncomeParadotea] = useState([]);
+    const [ekxorimena, setEkxorimena] = useState([]);
+    const [incomeTim, setIncomeTim] = useState([]);
+    const [filters, setFilters] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+    const [statuses] = useState(['Bank', 'Customer','Paradotea','Timologia']);
+    const [totalIncome, setTotalIncome] = useState(0);
 
-    const getParadotea = async() =>{
-        const response = await axios.get(`${apiBaseUrl}/paradotea`);
-        setParadotea(response.data);
+    const { user } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        fetchData();
+        setLoading(false);
+        initFilters();
+    }, []);
+
+    const fetchData = async () => {
+        await getEkxorimena();
+        await getIncomePar();
+        await getIncomeTim();
+    };
+
+    const getEkxorimena = async () => {
+        const response = await axios.get(`${apiBaseUrl}/ek_tim`);
+        setEkxorimena(response.data);
+    };
+
+    const getIncomePar = async () => {
+        const response = await axios.get(`${apiBaseUrl}/income_par`);
+        setIncomeParadotea(response.data);
+    };
+
+    const getIncomeTim = async () => {
+        const response = await axios.get(`${apiBaseUrl}/income_tim`);
+        setIncomeTim(response.data);
+    };
+
+    const clearFilter = () => {
+        initFilters();
+    };
+
+    const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
+        let _filters = { ...filters };
+        _filters['global'].value = value;
+        setFilters(_filters);
+        setGlobalFilterValue(value);
+    };
+
+    const initFilters = () => {
+        setFilters({
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+            income: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+            type: { operator: FilterOperator.OR, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+
+        });
+        setGlobalFilterValue('');
+    };
+
+    const formatDate = (value) => {
+        let date = new Date(value);
+        
+        if (!isNaN(date)) {
+            console.log("show date ",date.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            }))
+            return date.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            });
+        } else {
+            return "Invalid date";
+        }
+    };
+
+  //Sign Date
+  const DateBodyTemplate = (rowData) => {
+    return formatDate(rowData.date);
+};
+
+const dateFilterTemplate = (options) => {
+    console.log('Current filter value:', options);
+
+    return <Calendar value={options.value} onChange={(e) => options.filterCallback(e.value, options.index)} dateFormat="mm/dd/yy" placeholder="mm/dd/yyyy" mask="99/99/9999" />;
+};
+
+
+const formatCurrency = (value) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'EUR' });
+};
+
+const ammountBodyTemplate = (rowData) => {
+    return formatCurrency(rowData.income);
+};
+
+
+const ammountFilterTemplate = (options) => {
+    return <InputNumber value={options.value} onChange={(e) => options.filterCallback(e.value, options.index)} mode="currency" currency="EUR" locale="en-US" />;
+};
+
+
+const getSeverity = (status) => {
+    switch (status) {
+        case 'Bank':
+            return 'danger';
+
+        case 'Customer':
+            return 'success';
+        case 'Paradotea':
+            return 'danger';
+
+        case 'Timologia':
+            return 'success';
+     
     }
-    const deleteParadotea = async(ParadoteoId)=>{
-        await axios.delete(`${apiBaseUrl}/paradotea/${ParadoteoId}`);
-        getParadotea();
-    }
+};
 
-    return(
-        <div style={{ overflowX: 'auto', maxWidth: '800px'}}>
-        <h1 className='title'>Παραδοτέα</h1>
-        {user && user.role ==="admin" && (
-        <Link to={"/paradotea/add"} className='button is-primary mb-2'>Προσθήκη Νεου Παραδοτέου</Link>
-        )}
-        <table className='table is-stripped is-fullwidth'>
-            <thead>
-                <tr>
+const statusBodyTemplate = (rowData) => {
+    return <Tag value={rowData.type} severity={getSeverity(rowData.type)} />;
+};
 
-                    <th>#</th>
+const statusFilterTemplate = (options) => {
+    return <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterCallback(e.value, options.index)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear />;
+};
 
-                    <th>ΑΡΙΘΜΟΣ ΠΑΡΑΔΟΤΕΟΥ</th>
-                    <th>ΤΙΤΛΟΣ</th>
-                    <th>ΗΜΕΡΟΜΗΝΙΑ ΠΑΡΑΔΟΣΗΣ</th>
-                    <th>ΠΟΣΟΣΤΟ ΕΠΙ ΤΟΥ ΣΥΜΒΑΤΙΚΟΥ</th>
-                    <th>ΕΡΓΑ ID</th>
-                    <th>ΤΙΜΟΛΟΓΙΑ ID</th>
-                    <th>ΑΡΧΙΚΟ ΠΟΣΟ</th>
-                    <th>ΠΟΣΟ ΦΠΑ</th>
-                    <th>ΣΥΝΟΛΙΚΟ ΠΟΣΟ</th>
-                    <th>ΠΡΟΒΛΕΠΟΜΕΝΗ ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ 1</th>
-                    <th>ΠΡΟΒΛΕΠΟΜΕΝΗ ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ 2</th>
-                    <th>ΠΡΟΒΛΕΠΟΜΕΝΗ ΗΜΕΡΟΜΗΝΙΑ ΠΛΗΡΩΜΗΣ 3</th>
-                    <th>ΕΝΕΡΓΕΙΕΣ</th>
-                </tr>
-            </thead>
-            <tbody>
-                {paradotea.map((paradotea,index)=>(
-                    <tr key={paradotea.id}>
-                        <td>{index+1}</td>
-                        <td>{paradotea.part_number}</td>
-                        <td>{paradotea.title}</td>
-                        <td>{paradotea.delivery_date}</td>
-                        <td>{paradotea.percentage}</td>
-                        <td>{paradotea.erga_id}</td>
-                        <td>{paradotea.timologia_id }</td>
-                        <td>{paradotea.ammount}</td>
-                        <td>{paradotea.ammount_vat}</td>
-                        <td>{paradotea.ammount_total}</td>
-                        <td>{paradotea.estimate_payment_date}</td>
-                        <td>{paradotea.estimate_payment_date_2}</td>
-                        <td>{paradotea.estimate_payment_date_3}</td>
+const statusItemTemplate = (option) => {
+    return <Tag value={option} severity={getSeverity(option)} />;
+};
 
-                        <td>
-                            <Link to={`/paradotea/profile/${paradotea.id}`} className='button is-small is-info'>Προφίλ</Link>
-                            {user && user.role ==="admin" && (
-                            <div>
-                                <Link to={`/paradotea/edit/${paradotea.id}`} className='button is-small is-info'>Επεξεργασία</Link>
-                                <button onClick={()=>deleteParadotea(paradotea.id)} className='button is-small is-danger'>Διαγραφή</button>
-                            </div>
-                            )}
-                            
-                        </td>
-                    </tr>
-                ))}
-                
-            </tbody>
-        </table>
-    </div>
-    )
-}
+
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-between">
+                <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined onClick={clearFilter} />
+                <IconField iconPosition="left">
+                    <InputIcon className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                </IconField>
+            </div>
+        );
+    };
+
+    const calculateTotalIncome = (data) => {
+        if (!data || data.length === 0) return 0;
+
+        return data.reduce((acc, item) => acc + item.income, 0);
+    };
+    
+
+    const combinedData = [
+        ...ekxorimena.map(item => ({ date: new Date(item.bank_date), income: item.bank_ammount, type: 'Bank', id: item.id })),
+        ...ekxorimena.map(item => ({ date: new Date(item.customer_date), income: item.customer_ammount, type: 'Customer', id: item.id })),
+        ...paradotea.map(item => ({ date: new Date(item.paradotea.estimate_payment_date), income: item.paradotea.ammount_total, type: 'Paradotea', id: item.id })),
+        ...incomeTim.map(item => ({ date: new Date(item.timologia.actual_payment_date), income: item.timologia.ammount_of_income_tax_incl, type: 'Timologia', id: item.id }))
+    ];
+
+
+
+
+
+    useEffect(() => {
+        setTotalIncome(calculateTotalIncome(combinedData));
+    }, [combinedData]);
+
+    const handleFilter = (filteredData) => {
+        setTotalIncome(calculateTotalIncome(filteredData));
+    };
+
+    console.log(combinedData)
+
+
+    const header = renderHeader();
+
+    return (
+        <div>
+            <DataTable value={combinedData} paginator rows={10} 
+            header={header} 
+            filters={filters} 
+            filterDisplay="menu" loading={loading} 
+            responsiveLayout="scroll" 
+            globalFilterFields={['date', 'income', 'type','id']}
+            onFilter={(e) => handleFilter(e.filteredValue)}>
+                <Column filterField="date" header="date" dataType="date" style={{ minWidth: '5rem' }} body={DateBodyTemplate} filter filterElement={dateFilterTemplate}></Column>
+                <Column filterField="income" header="income" dataType="numeric" style={{ minWidth: '5rem' }} body={ammountBodyTemplate} filter filterElement={ammountFilterTemplate} footer={formatCurrency(totalIncome)}></Column>
+                <Column field="type" header="Type" filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '5rem' }} body={statusBodyTemplate} filter filterElement={statusFilterTemplate} />
+
+                <Column field="id" header="Id" filter sortable></Column>
+
+            </DataTable>
+        </div>
+    );
+};
 
 export default PaidList;
